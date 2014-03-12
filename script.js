30107d5fdb8ba53cb0883b04ee0ba3adcabdeb3f
elnoremedia <info@elnoremedia.com>
2014-03-12 16:17:09 +0100

    Test
    Test

--- script.js
+++ script.js
@@ -27,13 +27,13 @@
 	// create the controller and inject Angular's $scope
 	scotchApp.controller('mainController', function($scope) {
 		// create a message to display in our view
-		$scope.message = 'Everyone come and see how good I look!';
+		$scope.message = 'Homepagina tekst';
 	});
 
 	scotchApp.controller('aboutController', function($scope) {
-		$scope.message = 'Look! I am an about page.';
+		$scope.message = 'Over pagina tekst';
 	});
 
 	scotchApp.controller('contactController', function($scope) {
-		$scope.message = 'Contact us! JK. This is just a demo.';
+		$scope.message = 'Contactpagina tekst';
 	});
